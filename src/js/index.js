7f4eb35c26f6d91e10ee6c7ee19966d3b2a71825
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2018-01-03 22:55:36 -0200

    small changes for text

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
+import store from '../js/store/index';
+import { addArticle } from '../js/actions/index';
+
+window.store = store;
+window.addArticle = addArticle;
+
 console.log("I am a boilerplate :-)");
